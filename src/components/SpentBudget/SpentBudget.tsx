f06f49d7b4a3c6087bfe0925178efaeb853b54a7
Alina Kachur <alinakachur32@gmail.com>
2022-12-08 18:21:49 +0300

    feat(p_4):add overpayment calculations in SpentBudget

--- src/components/SpentBudget/SpentBudget.tsx
+++ src/components/SpentBudget/SpentBudget.tsx
@@ -1,12 +1,18 @@
 import { useCurrencyContext } from "context/CurrencyContext/CurrencyContext";
+import { useExpensesContext } from "context/ExpensesContext/ExpensesContext";
 import { StyledSpentBudget, TitleSpent } from "./styles";
 
 export const SpentBudget = () => {
   const { curentCurrency } = useCurrencyContext();
+  const { expenses } = useExpensesContext();
+
+  const spent = expenses.reduce((total, expense) => (total += expense.cost), 0);
 
   return (
     <StyledSpentBudget>
-      <TitleSpent>Spent so far: {curentCurrency.value}</TitleSpent>
+      <TitleSpent>
+        Spent so far: {curentCurrency.value} {spent}
+      </TitleSpent>
     </StyledSpentBudget>
   );
 };
